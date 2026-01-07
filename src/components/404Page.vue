5c5adfd7d755b0f165b2c07397ddb16fad5c20ea
Josh <jrheim267@gmail.com>
2026-01-07 01:13:49 +0100

    add 404 page

---
+++ src/components/404Page.vue
@@ -0,0 +1,20 @@
+<script>
+import ClassView from '@base/ClassView.vue'
+
+export default {
+  name: 'NotFound',
+  components: { ClassView },
+}
+</script>
+
+<template>
+  <ClassView>
+    <div class="flex justify-center items-center text-9xl text-white">
+      <div class="flex flex-col justify-center items-center">
+        <p>404</p>
+        <p>Not Found</p>
+        <p class="text-2xl">This Page is still being made.</p>
+      </div>
+    </div>
+  </ClassView>
+</template>
